d819ca8ad729324015adb5c606c0df65f8286a15
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-16 19:25:17 +0600

    throw problem is solved

---
+++ Day 3/throw.js
@@ -0,0 +1,62 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+
+// solution is here
+function isPositive(a) {
+    try {
+        if (a > 0) {
+            return "YES";
+        } 
+        throw new Error();
+    }
+    catch (err) {
+        if (a === 0) {
+            return err.message = "Zero Error"
+        } else {
+            return err.message = "Negative Error"
+        }
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    for (let i = 0; i < n; i++) {
+        const a = +(readLine());
+      
+        try {
+            console.log(isPositive(a));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
