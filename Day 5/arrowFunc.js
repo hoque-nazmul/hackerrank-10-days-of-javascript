955497c954e78d572ce1ec2a4fd67c1c7075f7c8
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-16 19:44:24 +0600

    arrow function problem is solved

---
+++ Day 5/arrowFunc.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+
+// solution is here
+function modifyArray(nums) {
+    const modifyArray = nums.map(element => {
+        if (element % 2 === 0) {
+            return element * 2;
+        }
+        return element * 3;
+    });
+    return modifyArray
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
