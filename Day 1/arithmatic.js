e72b8adce6719c31acf09996da48cfc8b317b45d
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-16 18:49:34 +0600

    arithmatic operator is solved

---
+++ Day 1/arithmatic.js
@@ -0,0 +1,69 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+
+// solution is here
+function getArea(length, width) {
+    // Write your code here
+    let area = length * width;
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+
+// solution is here
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    let add = length + width;
+
+    perimeter = 2 * add;
+    
+    return perimeter;
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
